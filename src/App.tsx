c957de8a98022aef131480aecbf5a8ba30c21ef4
tmktmk <makremprt@gmail.com>
2026-04-16 19:54:31 +0100

    fix: add trailing slash route for GitHub Pages blog URLs

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => (
         <Route path="/" element={<Index />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/blog/:slug/" element={<BlogPost />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
